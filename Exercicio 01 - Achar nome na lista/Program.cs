6168882bf9d310dce660dbc22eed2726201e4d7b
Luiz Fernando <luizfernandolima1997@gmail.com>
2023-02-21 14:58:38 -0300

    atividade encontrar nome na lista

---
+++ Exercicio 01 - Achar nome na lista/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+Console.Write("Digite o nome para busca: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("======================");
+
+bool NaLista()
+{
+    for (int i = 0; i < nomesDosEscolhidos.Count; i++)
+    {
+        if (nome.Equals(nomesDosEscolhidos[i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+if (NaLista())
+{
+    Console.WriteLine($"Nome {nome} esta na lista :)");
+}
+else
+{
+    Console.WriteLine($"Nome {nome} não está na lista :(");
+}
+
+Console.ReadLine();
