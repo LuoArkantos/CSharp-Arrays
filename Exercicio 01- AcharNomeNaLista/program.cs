a256556d381db4ed6c2d8f2d874e5a37907d4a8f
Luiz Fernando <110204745+LuoArkantos@users.noreply.github.com>
2023-02-21 09:23:06 -0300

    iniciando codigo

--- Exercicio 01- AcharNomeNaLista/program.cs
+++ Exercicio 01- AcharNomeNaLista/program.cs
@@ -1,1 +1,27 @@
 using System;
+
+List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+void BuscarNome()
+{
+    string nomeBuscado = Console.Readline();
+    string nomeEncontrado = nomeDosEscolhidos.Where("Uxas").ToString;
+    Console.WriteLine();
+
+}
+BuscarNome();
+Console.Readline();
